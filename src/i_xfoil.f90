9297eb858a6bf4673e97976fb62740e9dc253382
Daniel de Vries <daniel.devries@darcorp.com>
2019-01-11 11:08:59 -0600

    Removed COMMON statements from i_xfoil.f90

--- src/i_xfoil.f90
+++ src/i_xfoil.f90
@@ -21,168 +21,106 @@ module i_xfoil
     !---- include polar variable indexing parameters
     use i_pindex
     !
-    PARAMETER (IQX = 370, IPX = 5, ISX = 2)
-    PARAMETER (IWX = IQX / 8 + 2)
-    PARAMETER (IBX = 4 * IQX)
-    PARAMETER (IZX = IQX + IWX)
-    PARAMETER (IVX = IQX / 2 + IWX + 50)
-    PARAMETER (NAX = 800, NPX = 12, NFX = 128)
-    PARAMETER (NTX = 2 * IBX)
-    CHARACTER*32 LABREF
-    CHARACTER*64 FNAME, PFNAME, PFNAMX, ONAME, PREFIX, OCNAME
-    CHARACTER*48 NAME, NAMEPOL, CODEPOL, NAMEREF
-    CHARACTER*80 ISPARS
-    LOGICAL OK, LIMAGE, &
-            LGAMU, LQINU, SHARP, LVISC, LALFA, LWAKE, LPACC, &
-            LBLINI, LIPAN, LQAIJ, LADIJ, LWDIJ, LCPXX, LQVDES, LQREFL, &
-            LQSPEC, LVCONV, LCPREF, LCLOCK, LPFILE, LPFILX, LPPSHO, &
-            LBFLAP, LFLAP, LEIW, LSCINI, LFOREF, LNORM, LGSAME, LDCPLOT, &
-            LPLCAM, LQSYM, LGSYM, LQGRID, LGGRID, LGTICK, &
-            LQSLOP, LGSLOP, LCSLOP, LQSPPL, LGEOPL, LGPARM, &
-            LCPGRD, LBLGRD, LBLSYM, LCMINP, LHMOMP, LFREQP, &
-            LCPINV
-    LOGICAL LPLOT, LSYM, LIQSET, LCLIP, LVLAB, LCURS, LLAND
-    LOGICAL LPGRID, LPCDW, LPLIST, LPLEGN, LAECEN, LPCDH, LPCMDOT
-    LOGICAL TFORCE
-    REAL NX, NY, MASS, MINF1, MINF, MINF_CL, MVISC, MACHP1
-    INTEGER RETYP, MATYP, AIJPIV
-    CHARACTER*1 VMXBL
+    integer , parameter  ::  IQX = 370 , IPX = 5 , ISX = 2 , IWX = IQX/8 + 2 , IBX = 4*IQX , IZX = IQX + IWX ,        &
+            &                         IVX = IQX/2 + IWX + 50 , NAX = 800 , NPX = 12 , NFX = 128 , NTX = 2*IBX
     !
-    !---- dimension temporary work and storage arrays (EQUIVALENCED below)
-    COMMON W1(6 * IQX), W2(6 * IQX), W3(6 * IQX), W4(6 * IQX), &
-            W5(6 * IQX), W6(6 * IQX), W7(6 * IQX), W8(6 * IQX)
-    COMMON BIJ(IQX, IZX), CIJ(IWX, IQX)
-    !
-    COMMON/CR01/ VERSION
-    COMMON/CC01/ FNAME, &
-            NAME, ISPARS, ONAME, PREFIX, OCNAME, &
-            PFNAME(NPX), PFNAMX(NPX), &
-            NAMEPOL(NPX), CODEPOL(NPX), &
-            NAMEREF(NPX)
-    COMMON/QMAT/ Q(IQX, IQX), DQ(IQX), &
-            DZDG(IQX), DZDN(IQX), DZDM(IZX), &
-            DQDG(IQX), DQDM(IZX), QTAN1, QTAN2, &
-            Z_QINF, Z_ALFA, Z_QDOF0, Z_QDOF1, Z_QDOF2, Z_QDOF3
-    COMMON/CR03/ AIJ(IQX, IQX), DIJ(IZX, IZX)
-    COMMON/CR04/ QINV(IZX), QVIS(IZX), CPI(IZX), CPV(IZX), &
-            QINVU(IZX, 2), QINV_A(IZX)
-    COMMON/CR05/ X(IZX), Y(IZX), XP(IZX), YP(IZX), S(IZX), &
-            SLE, XLE, YLE, XTE, YTE, CHORD, YIMAGE, &
-            WGAP(IWX), WAKLEN
-    COMMON/CR06/ GAM(IQX), GAMU(IQX, 2), GAM_A(IQX), SIG(IZX), &
-            NX(IZX), NY(IZX), APANEL(IZX), &
-            SST, SST_GO, SST_GP, &
-            GAMTE, GAMTE_A, &
-            SIGTE, SIGTE_A, &
-            DSTE, ANTE, ASTE
-    COMMON/CR07/ SSPLE, &
-            SSPEC(IBX), XSPOC(IBX), YSPOC(IBX), &
-            QGAMM(IBX), &
-            QSPEC(IBX, IPX), QSPECP(IBX, IPX), &
-            ALGAM, CLGAM, CMGAM, &
-            ALQSP(IPX), CLQSP(IPX), CMQSP(IPX), &
-            QF0(IQX), QF1(IQX), QF2(IQX), QF3(IQX), &
-            QDOF0, QDOF1, QDOF2, QDOF3, CLSPEC, FFILT
-    COMMON/CI01/ IQ1, IQ2, NSP, NQSP, KQTARG, IACQSP, NC1, NNAME, NPREFIX
-    COMMON/CR09/ ADEG, ALFA, AWAKE, MVISC, AVISC, &
-            XCMREF, YCMREF, &
-            CL, CM, CD, CDP, CDF, CL_ALF, CL_MSQ, &
-            PSIO, CIRC, COSA, SINA, QINF, &
-            GAMMA, GAMM1, &
-            MINF1, MINF, MINF_CL, TKLAM, TKL_MSQ, CPSTAR, QSTAR, &
-            CPMN, CPMNI, CPMNV, XCPMNI, XCPMNV
-    COMMON/CI03/ NCPREF, NAPOL(NPX), NPOL, IPACT, NLREF, &
-            ILINP(NPX), ICOLP(NPX), &
-            ISYMR(NPX), ICOLR(NPX), &
-            IMATYP(NPX), IRETYP(NPX), NXYPOL(NPX), &
-            NPOLREF, NDREF(4, NPX), &
-            IPOL(IPTOT), NIPOL, NIPOL0, &
-            JPOL(JPTOT), NJPOL
-    COMMON/CR10/ XPREF(IQX), CPREF(IQX), VERSPOL(NPX), &
-            CPOL(NAX, IPTOT, NPX), &
-            CPOLSD(NAX, ISX, JPTOT, NPX), &
-            CPOLXY(IQX, 2, NPX), &
-            MACHP1(NPX), &
-            REYNP1(NPX), &
-            ACRITP(ISX, NPX), &
-            PTRATP(NPX), &
-            ETAPP(NPX), &
-            XSTRIPP(ISX, NPX), &
-            CPOLREF(NFX, 2, 4, NPX)
-    COMMON/CC02/ LABREF
-    !
-    COMMON/CR11/ PI, HOPI, QOPI, DTOR
-    COMMON/CR12/ CVPAR, CTERAT, CTRRAT, XSREF1, XSREF2, XPREF1, XPREF2
-    COMMON/CI04/ N, NB, NW, NPAN, IST, KIMAGE, KDELIM, &
-            ITMAX, NSEQEX, RETYP, MATYP, AIJPIV(IQX), &
-            IDEV, IDEVRP, IPSLU, NCOLOR, &
-            ICOLS(ISX), NOVER, NCM, NTK
-    COMMON/CR13/ SIZE, SCRNFR, PLOTAR, PFAC, UFAC, QFAC, VFAC, &
-            XWIND, YWIND, &
-            XPAGE, YPAGE, XMARG, YMARG, &
-            CH, CHG, CHQ, &
-            XOFAIR, YOFAIR, FACAIR, XOFA, YOFA, FACA, UPRWT, &
-            CPMIN, CPMAX, CPDEL, &
-            UEMIN, UEMAX, UEDEL, &
-            CPOLPLF(3, 4), &
-            XCDWID, XALWID, XOCWID
-    COMMON/CL01/ OK, LIMAGE, SHARP, &
-            LGAMU, LQINU, LVISC, LALFA, LWAKE, LPACC, &
-            LBLINI, LIPAN, LQAIJ, LADIJ, LWDIJ, LCPXX, LQVDES, LQREFL, &
-            LQSPEC, LVCONV, LCPREF, LCLOCK, LPFILE, LPFILX, LPPSHO, &
-            LBFLAP, LFLAP, LEIW, LSCINI, LFOREF, LNORM, LGSAME, LDCPLOT, &
-            LPLCAM, LQSYM, LGSYM, &
-            LQGRID, LGGRID, LGTICK, &
-            LQSLOP, LGSLOP, LCSLOP, LQSPPL, LGEOPL, LGPARM, &
-            LCPGRD, LBLGRD, LBLSYM, &
-            LPLOT, LSYM, LIQSET, LCLIP, LVLAB, LCURS, LLAND, &
-            LPGRID, LPCDW, LPLIST, LPLEGN, LAECEN, LPCDH, LPCMDOT, &
-            LCMINP, LHMOMP, LFREQP, &
-            LCPINV
-    COMMON/CR14/ XB(IBX), YB(IBX), &
-            XBP(IBX), YBP(IBX), SB(IBX), SNEW(5 * IBX), &
-            XBF, YBF, XOF, YOF, HMOM, HFX, HFY, &
-            XBMIN, XBMAX, YBMIN, YBMAX, &
-            SBLE, CHORDB, AREAB, RADBLE, ANGBTE, &
-            EI11BA, EI22BA, APX1BA, APX2BA, &
-            EI11BT, EI22BT, APX1BT, APX2BT, &
-            THICKB, CAMBRB, &
-            XCM(2 * IBX), YCM(2 * IBX), SCM(2 * IBX), XCMP(2 * IBX), YCMP(2 * IBX), &
-            XTK(2 * IBX), YTK(2 * IBX), STK(2 * IBX), XTKP(2 * IBX), YTKP(2 * IBX)
-    !
-    COMMON/CR15/ XSSI(IVX, ISX), UEDG(IVX, ISX), UINV(IVX, ISX), &
-            MASS(IVX, ISX), THET(IVX, ISX), DSTR(IVX, ISX), &
-            CTAU(IVX, ISX), DELT(IVX, ISX), TSTR(IVX, ISX), &
-            USLP(IVX, ISX), GUXQ(IVX, ISX), GUXD(IVX, ISX), &
-            TAU(IVX, ISX), DIS(IVX, ISX), CTQ(IVX, ISX), &
-            VTI(IVX, ISX), &
-            REINF1, REINF, REINF_CL, &
-            ACRIT(ISX), &
-            XSTRIP(ISX), XOCTR(ISX), YOCTR(ISX), XSSITR(ISX), &
-            UINV_A(IVX, ISX), &
-            TINDEX(ISX)
-    COMMON/CI05/ IXBLP, &
-            IBLTE(ISX), NBL(ISX), IPAN(IVX, ISX), ISYS(IVX, ISX), NSYS, &
-            ITRAN(ISX), IDAMP
-    COMMON/CL02/ TFORCE(ISX)
-    COMMON/CR17/ RMSBL, RMXBL, RLX, VACCEL
-    COMMON/CI06/ IMXBL, ISMXBL
-    COMMON/CC03/ VMXBL
-    COMMON/CR18/ XSF, YSF, XOFF, YOFF, &
-            XGMIN, XGMAX, YGMIN, YGMAX, DXYG, &
-            XCMIN, XCMAX, YCMIN, YCMAX, DXYC, DYOFFC, &
-            XPMIN, XPMAX, YPMIN, YPMAX, DXYP, DYOFFP, &
-            YSFP, GTICK
-    COMMON/CR19/&
-            XCADD(NTX), YCADD(NTX), YCADDP(NTX), &
-            XPADD(NTX), YPADD(NTX), YPADDP(NTX), &
-            XCAM(NTX), &
-            YCAM(NTX), YCAMP(NTX), &
-            PCAM(NTX), PCAMP(NTX)
-    COMMON/CI19/ NCAM
-    !
-    COMMON/VMAT/ VA(3, 2, IZX), VB(3, 2, IZX), VDEL(3, 2, IZX), &
-            VM(3, IZX, IZX), VZ(3, 2)
+    !---- dimension temporary work and storage arrays
+    real , dimension(IQX,IZX)  ::  BIJ
+    real , dimension(IWX,IQX)  ::  CIJ
+    real , dimension(6*IQX)  ::  W1 , W2 , W3 , W4 , W5 , W6 , W7 , W8
+
+    character(48) , dimension(NPX)  ::  CODepol , NAMepol , NAMeref
+    character(64)  ::  FNAme , OCName , ONAme , PREfix
+    character(80)  ::  ISPars
+    character(48)  ::  NAMe
+    character(64) , dimension(NPX)  ::  PFName , PFNamx
+    character(32)  ::  LABref
+    character(1)  ::  VMXbl
+
+    integer  ::  IACqsp , IQ1 , IQ2 , KQTarg , NC1 , NNAme , NPRefix , NQSp , NSP
+
+    integer , dimension(NPX)  ::  ICOlp , ICOlr , ILInp , IMAtyp , IREtyp , ISYmr , NAPol , NXYpol
+    integer  ::  IPAct , NCPref , NIPol , NIPol0 , NJPol , NLRef , NPOl , NPOlref
+    integer , dimension(IPTOT)  ::  IPOl
+    integer , dimension(JPTOT)  ::  JPOl
+    integer , dimension(4,NPX)  ::  NDRef
+
+    integer , dimension(IQX)  ::  AIJpiv
+    integer , dimension(ISX)  ::  ICOls
+    integer  ::  IDEv , IDEvrp , IPSlu , IST , ITMax , KDElim , KIMage , MATyp , N , NB , NCM , NCOlor , NOVer ,      &
+            &             NPAn , NSEqex , NTK , NW , RETyp
+
+    integer , dimension(ISX)  ::  IBLte , ITRan , NBL
+    integer  ::  IDAmp , IXBlp , NSYs
+    integer , dimension(IVX,ISX)  ::  IPAn , ISYs
+
+    integer  ::  IMXbl , ISMxbl
+    integer  ::  NCAm
+
+    logical  ::  LADij , LAEcen , LALfa , LBFlap , LBLgrd , LBLini , LBLsym , LCLip , LCLock , LCMinp , LCPgrd ,      &
+            &             LCPinv , LCPref , LCPxx , LCSlop , LCUrs , LDCplot , LEIw , LFLap , LFOref , LFReqp , LGAmu ,        &
+            &             LGEopl , LGGrid , LGParm , LGSame , LGSlop , LGSym , LGTick , LHMomp , LIMage , LIPan , LIQset ,     &
+            &             LLAnd , LNOrm , LPAcc , LPCdh , LPCdw , LPCmdot , LPFile , LPFilx , LPGrid , LPLcam , LPLegn ,       &
+            &             LPList , LPLot , LPPsho , LQAij , LQGrid , LQInu , LQRefl , LQSlop , LQSpec , LQSppl , LQSym ,       &
+            &             LQVdes , LSCini , LSYm , LVConv , LVIsc , LVLab , LWAke , LWDij , OK , SHArp
+    logical , dimension(ISX)  ::  TFOrce
+
+    real  ::  VERsion
+    real , dimension(IQX,IQX)  ::  AIJ
+    real , dimension(IZX,IZX)  ::  DIJ
+    real , dimension(IZX)  ::  CPI , CPV , QINv , QINv_a , QVIs
+    real , dimension(IZX,2)  ::  QINvu
+    real  ::  CHOrd , SLE , WAKlen , XLE , XTE , YIMage , YLE , YTE
+    real , dimension(IZX)  ::  S , X , XP , Y , YP
+    real , dimension(IWX)  ::  WGAp
+    real  ::  ANTe , ASTe , DSTe , GAMte , GAMte_a , SIGte , SIGte_a , SST , SST_go , SST_gp
+    real , dimension(IZX)  ::  APAnel , NX , NY , SIG
+    real , dimension(IQX)  ::  GAM , GAM_a
+    real , dimension(IQX,2)  ::  GAMu
+    real  ::  ALGam , CLGam , CLSpec , CMGam , FFIlt , QDOf0 , QDOf1 , QDOf2 , QDOf3 , SSPle
+    real , dimension(IPX)  ::  ALQsp , CLQsp , CMQsp
+    real , dimension(IQX)  ::  QF0 , QF1 , QF2 , QF3
+    real , dimension(IBX)  ::  QGAmm , SSPec , XSPoc , YSPoc
+    real , dimension(IBX,IPX)  ::  QSPec , QSPecp
+    real  ::  ADEg , ALFa , AVIsc , AWAke , CD , CDF , CDP , CIRc , CL , CL_alf , CL_msq , CM , COSa , CPMn , CPMni , &
+            &          CPMnv , CPStar , GAMm1 , GAMma , MINf , MINf1 , MINf_cl , MVIsc , PSIo , QINf , QSTar , SINa , TKLam ,  &
+            &          TKL_msq , XCMref , XCPmni , XCPmnv , YCMref
+    real , dimension(ISX,NPX)  ::  ACRitp , XSTripp
+    real , dimension(NAX,IPTOT,NPX)  ::  CPOl
+    real , dimension(NFX,2,4,NPX)  ::  CPOlref
+    real , dimension(NAX,ISX,JPTOT,NPX)  ::  CPOlsd
+    real , dimension(IQX,2,NPX)  ::  CPOlxy
+    real , dimension(IQX)  ::  CPRef , XPRef
+    real , dimension(NPX)  ::  ETApp , MAChp1 , PTRatp , REYnp1 , VERspol
+    real  ::  DTOr , HOPi , PI , QOPi
+    real  ::  CTErat , CTRrat , CVPar , XPRef1 , XPRef2 , XSRef1 , XSRef2
+    real  ::  CH , CHG , CHQ , CPDel , CPMax , CPMin , FACa , FACair , PFAc , PLOtar , QFAc , SCRnfr , SIZe , UEDel , &
+            &          UEMax , UEMin , UFAc , UPRwt , VFAc , XALwid , XCDwid , XMArg , XOCwid , XOFa , XOFair , XPAge , XWInd ,&
+            &          YMArg , YOFa , YOFair , YPAge , YWInd
+    real , dimension(3,4)  ::  CPOlplf
+    real  ::  ANGbte , APX1ba , APX1bt , APX2ba , APX2bt , AREab , CAMbrb , CHOrdb , EI11ba , EI11bt , EI22ba ,       &
+            &          EI22bt , HFX , HFY , HMOm , RADble , SBLe , THIckb , XBF , XBMax , XBMin , XOF , YBF , YBMax , YBMin ,  &
+            &          YOF
+    real , dimension(IBX)  ::  SB , XB , XBP , YB , YBP
+    real , dimension(2*IBX)  ::  SCM , STK , XCM , XCMp , XTK , XTKp , YCM , YCMp , YTK , YTKp
+    real , dimension(5*IBX)  ::  SNEw
+    real , dimension(ISX)  ::  ACRit , TINdex , XOCtr , XSSitr , XSTrip , YOCtr
+    real , dimension(IVX,ISX)  ::  CTAu , CTQ , DELt , DIS , DSTr , GUXd , GUXq , MASs , TAU , THEt , TSTr , UEDg ,   &
+            &                               UINv , UINv_a , USLp , VTI , XSSi
+    real  ::  REInf , REInf1 , REInf_cl
+    real  ::  RLX , RMSbl , RMXbl , VACcel
+    real  ::  DXYc , DXYg , DXYp , DYOffc , DYOffp , GTIck , XCMax , XCMin , XGMax , XGMin , XOFf , XPMax , XPMin ,   &
+            &          XSF , YCMax , YCMin , YGMax , YGMin , YOFf , YPMax , YPMin , YSF , YSFp
+    real , dimension(NTX)  ::  PCAm , PCAmp , XCAdd , XCAm , XPAdd , YCAdd , YCAddp , YCAm , YCAmp , YPAdd , YPAddp
+
+    real , dimension(IQX)  ::  DQ , DQDg , DZDg , DZDn
+    real , dimension(IZX)  ::  DQDm , DZDm
+    real , dimension(IQX,IQX)  ::  Q
+    real  ::  QTAn1 , QTAn2 , Z_Alfa , Z_Qdof0 , Z_Qdof1 , Z_Qdof2 , Z_Qdof3 , Z_Qinf
+
+    real , dimension(3,2,IZX)  ::  VA , VB , VDEl
+    real , dimension(3,IZX,IZX)  ::  VM
+    real , dimension(3,2)  ::  VZ
     !
     !
     !
